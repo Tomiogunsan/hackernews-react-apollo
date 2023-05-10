b8c88dc46eb118534b6df5c5c7bc2b5190f7d18b
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-05-10 10:19:04 +0100

    created LinkList components

---
+++ src/components/Link.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Link({link}) {
+    
+  return (
+    <div>
+        <div>
+            {link.description} ({link.url})
+        </div>
+    </div>
+  )
+}
